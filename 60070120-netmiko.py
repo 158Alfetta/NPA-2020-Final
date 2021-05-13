e1db04a674fd4cd57b7e9b29b03f529847a62e20
158Alfetta <60070120@kmitl.ac.th>
2021-05-13 02:44:31 +0000

    Commit First Task

---
+++ 60070120-netmiko.py
@@ -0,0 +1,20 @@
+import netmiko
+
+DEVICE_PARAMS = {
+                'device_type':'cisco_ios',
+                'ip':"10.0.15.104",
+                'username':'admin',
+                'password':'cisco'
+}
+
+def setLbInt(device_params):
+    ssh = netmiko.ConnectHandler(**device_params)
+    result = ssh.send_config_set([
+        'interface loopback 60070120',
+        'ip address 192.168.1.1 255.255.255.0',
+        'description testingNetmikoConfigLoopback'
+        ])
+    print(result)
+    ssh.disconnect()
+    
+setLbInt(DEVICE_PARAMS)
