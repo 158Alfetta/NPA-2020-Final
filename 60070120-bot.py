4b93df039f336852fed349c1df14bc2d7ca7bf05
158Alfetta <32899555+158Alfetta@users.noreply.github.com>
2021-05-13 10:48:48 +0700

    update comment

--- 60070120-bot.py
+++ 60070120-bot.py
@@ -1,24 +1,28 @@
 import json
 import requests
+import time
 from prettyprinter import pprint
 requests.packages.urllib3.disable_warnings()
 
+#define a token, both room and access token
 ACCESS_TOKEN = ""
 ROOM_ID = ""
 
+#use get method on RESTCONF protocol to retrive data from interfaces-state container of ietf-interfaces
 def getData():
-
     api_url = "https://10.0.15.104/restconf/data/ietf-interfaces:interfaces-state"
     headers = { "Accept": "application/yang-data+json",
     "Content-type":"application/yang-data+json"
     }
     basicauth = ("admin", "cisco")
 
+    #start a request
     resp = requests.get(api_url, auth=basicauth, headers=headers, verify=False)
     response_json = resp.json() 
     # this output is already dictionary
     interface = response_json['ietf-interfaces:interfaces-state']['interface']
     
+    #define an empty string to make a retun statement
     lb_data = ""
 
     for interface in response_json['ietf-interfaces:interfaces-state']['interface']:
@@ -27,6 +31,7 @@ def getData():
             pprint(lb_data)
     return lb_data
 
+# this is a function to get every room detail, using for get room ID
 def getRoomID():
     url = 'https://webexapis.com/v1/rooms'
     headers = {
@@ -38,7 +43,7 @@ def getRoomID():
     print(res.json())
 # getRoomID()
     
-
+#retrieve message of current room (a room that already define ROOM_ID)
 def retrieveMsg():
     webex_url = "https://webexapis.com/v1/messages"
     webex_auth = {"Content-Type":"application/json", "Authorization":"Bearer {}".format(ACCESS_TOKEN)}
@@ -46,6 +51,7 @@ def retrieveMsg():
     webex_response = requests.get(url=webex_url, headers=webex_auth, params=webex_param).json()
     return webex_response['items'][0]['text']
 
+#send message to current room (a room that already define ROOM_ID)
 def senderMsg(text):
     webex_url = "https://webexapis.com/v1/messages"
     webex_auth = {"Content-Type":"application/json", "Authorization":"Bearer {}".format(ACCESS_TOKEN)}
@@ -53,8 +59,10 @@ def senderMsg(text):
     webex_response = requests.post(url=webex_url, headers=webex_auth, json=webex_param).json()
     print(webex_response)
 
+#Operation are here.
 def main():
     while 1:
+        time.sleep(1)
         msg = retrieveMsg()
         print(msg)
         if msg == "60070120":
